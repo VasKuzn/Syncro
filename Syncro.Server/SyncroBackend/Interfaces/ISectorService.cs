66f629b26818dd735a23802aa15b63cc717eda99
Кузнецов Василий <vasilekkuz@mail.ru>
2025-06-01 01:00:42 +0500

    add: добавил логику и окружение секторов сервера

---
+++ Syncro.Server/SyncroBackend/Interfaces/ISectorService.cs
@@ -0,0 +1,12 @@
+namespace SyncroBackend.Interfaces
+{
+    public interface ISectorService
+    {
+        Task<List<SectorModel>> GetAllSectorsAsync();
+        Task<List<SectorModel>> GetSectorsByServerIdAsync(Guid serverId);
+        Task<SectorModel> GetSectorByIdAsync(Guid sectorId);
+        Task<SectorModel> CreateSectorAsync(SectorModel sector);
+        Task<bool> DeleteSectorAsync(Guid sectorId);
+        Task<SectorModel> UpdateSectorAsync(Guid sectorId, SectorModelDto sectorDto);
+    }
+}
