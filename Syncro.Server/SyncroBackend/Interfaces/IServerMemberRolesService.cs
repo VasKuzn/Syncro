d8a29e9e5a24fadd07a3b1ebd3926172f13b75fc
Кузнецов Василий <vasilekkuz@mail.ru>
2025-06-01 00:23:21 +0500

    add: добавил server member roles - связку ролей и участников сервера - добавил окружение и логику

---
+++ Syncro.Server/SyncroBackend/Interfaces/IServerMemberRolesService.cs
@@ -0,0 +1,11 @@
+namespace SyncroBackend.Interfaces
+{
+    public interface IServerMemberRolesService
+    {
+        Task<List<ServerMemberRoles>> GetMemberRolesAsync(Guid serverId, Guid accountId);
+        Task<List<ServerMemberRoles>> GetRoleAssignmentsAsync(Guid roleId);
+        Task<ServerMemberRoles> AssignRoleToMemberAsync(ServerMemberRoles memberRole);
+        Task<bool> RemoveRoleFromMemberAsync(Guid assignmentId);
+        Task<bool> RemoveAllRolesFromMemberAsync(Guid serverId, Guid accountId);
+    }
+}
