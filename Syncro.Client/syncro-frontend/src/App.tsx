a0931934e624cee50ce3840d0c888efd818e5b06
Populia0 <populia000@gmail.com>
2025-06-20 09:46:48 +0500

    add styles

--- Syncro.Client/syncro-frontend/src/App.tsx
+++ Syncro.Client/syncro-frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
+import Landing from "./Pages/Landing";
+import Chat from "./Pages/Chat";
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="/chat" element={<Chat />} />
       </Routes>
     </Router>
   );
