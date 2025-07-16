2336e2cfe0e71081a4f96e3b51f3161d94b9ed9f
Кузнецов Василий <vasilekkuz@mail.ru>
2025-07-16 15:04:56 +0500

    add: добавлена передача friendId при нажатии. Обозначен план действий в chat.tsx

--- Syncro.Client/syncro-frontend/src/Components/MainPage/PersonalChatsComponent.tsx
+++ Syncro.Client/syncro-frontend/src/Components/MainPage/PersonalChatsComponent.tsx
@@ -11,7 +11,7 @@ const PersonalChatsComponent = ({ friends }: FriendProps) => {
             </div>
             <div className="pc-list">
                 {friends.map(friend => (
-                    <div key={friend.id} className="pc-item" onClick={e => { navigate("/chat", { state: { friends } }); }}>
+                    <div key={friend.id} className="pc-item" onClick={e => { navigate("/chat", { state: { friends, friendId: friend.id } }); }}>
                         <div className="friend-info-container">
                             <div className="friend-avatar-container">
                                 <img className="friend-avatar" src={"/logo.png"}></img>
