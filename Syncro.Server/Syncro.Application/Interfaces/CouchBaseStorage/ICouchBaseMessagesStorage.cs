acc13e55e7e1b04eee1c0bf404da825075aca760
Populia0 <populia000@gmail.com>
2026-01-11 20:13:00 +0500

    add: функция для чтения сообщений

--- Syncro.Server/Syncro.Application/Interfaces/CouchBaseStorage/ICouchBaseMessagesStorage.cs
+++ Syncro.Server/Syncro.Application/Interfaces/CouchBaseStorage/ICouchBaseMessagesStorage.cs
@@ -1,13 +1,17 @@
-public interface ICouchBaseMessagesService
+namespace Syncro.Application.Interfaces.CouchBaseStorage
 {
-    Task<List<MessageModel>> GetAllMessagesAsync();
-    Task<MessageModel> GetMessageByIdAsync(Guid messageId);
-    Task<MessageModel> CreateMessageAsync(MessageModel message);
-    Task<bool> DeleteMessageAsync(Guid messageId);
-    Task<MessageModel> UpdateMessageTextAsync(Guid messageId, MessageDTO messageDTO);
-    Task<MessageModel> MarkMessageAsReadAsync(Guid messageId, Guid readerId, bool isRead);
-    Task<MessageModel> SetMessageReferenceAsync(Guid messageId, Guid referencedMessageId);
-    Task<MessageModel> ToggleMessagePinAsync(Guid messageId);
-    Task<List<MessageModel>> GetAllMessagesByPersonalConferenceAsync(Guid personalConferenceId);
-    Task<MessageModel> UpdateMessageAsync(MessageModel message);
+    public interface ICouchBaseMessagesService
+    {
+        Task<List<MessageModel>> GetAllMessagesAsync();
+        Task<MessageModel> GetMessageByIdAsync(Guid messageId);
+        Task<MessageModel> CreateMessageAsync(MessageModel message);
+        Task<bool> DeleteMessageAsync(Guid messageId);
+        Task<MessageModel> UpdateMessageTextAsync(Guid messageId, MessageDTO messageDTO);
+        Task<MessageModel> MarkMessageAsReadAsync(Guid messageId, Guid readerId, bool isRead);
+        Task<IEnumerable<MessageModel>> MarkMessagesAsReadAsync(Guid personalConferenceId);
+        Task<MessageModel> SetMessageReferenceAsync(Guid messageId, Guid referencedMessageId);
+        Task<MessageModel> ToggleMessagePinAsync(Guid messageId);
+        Task<List<MessageModel>> GetAllMessagesByPersonalConferenceAsync(Guid personalConferenceId);
+        Task<MessageModel> UpdateMessageAsync(MessageModel message);
+    }
 }
